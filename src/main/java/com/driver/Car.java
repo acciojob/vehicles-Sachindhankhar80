1417f7a7dd9fda67eaa266a76b6bef473d3e45ca
Sachin Dhankhar <sdhankhar940@gmail.com>
2023-07-07 16:33:22 +0530

    Answer

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,6 +11,7 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
         this.currentGear=1;
         this.wheels=wheels;
         this.type=type;
